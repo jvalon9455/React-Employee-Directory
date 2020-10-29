5b8f6da519431630b7450d98810e59a1aac8552c
John Alonso <jvalon9455@gmail.com>
2020-10-29 16:30:05 -0400

    added header component and styling

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div class="header">
+      <h1>Employee Directory</h1>
+      <br />
+      <h6>
+        Click on each heading to filter or use search box to narrow employee
+        results
+      </h6>
+    </div>
+  );
+};
+
+export default Header;
