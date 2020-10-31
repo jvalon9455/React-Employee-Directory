ecf0bec658d36a70d54dcf1edb01fe332979bc2a
John Alonso <jvalon9455@gmail.com>
2020-10-31 00:42:15 -0400

    added a table to help bring in api employees

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
   return (
     <div>
     <Header />
-    <Table />
     <Search />
+    <Table />
     </div>
   );
 }
---
+++ src/components/Table/Table.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+const Table = () => {
+    return (
+        <div className="table">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">Image</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Phone</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">DOB</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td>Larry</td>
+                        <td>the Bird</td>
+                        <td>@twitter</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default Table;
